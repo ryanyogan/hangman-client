a072ad5fd297f2e4e5677cc787d539a0e925e5c2
Ryan Yogan <ryanyogan@gmail.com>
2020-12-27 16:25:07 -0600

    support the cleaner API

--- lib/text_client/mover.ex
+++ lib/text_client/mover.ex
@@ -2,12 +2,11 @@ defmodule TextClient.Mover do
   alias TextClient.GameState
 
   def make_move(game) do
-    {gs, tally} = Hangman.make_move(game.game_service, game.guess)
+    tally = Hangman.make_move(game.game_service, game.guess)
 
     %GameState{
       game
-      | game_service: gs,
-        tally: tally
+      | tally: tally
     }
   end
 end
