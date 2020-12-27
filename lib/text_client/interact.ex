5b8ebd8236aecd137219469949a85895a0266b69
Ryan Yogan <ryanyogan@gmail.com>
2020-12-27 16:40:44 -0600

    do not run the server on the client node

--- lib/text_client/interact.ex
+++ lib/text_client/interact.ex
@@ -1,8 +1,9 @@
 defmodule TextClient.Interact do
   alias TextClient.{GameState, Player}
 
+  @hangman_server :hangman@orion
   def start() do
-    Hangman.new_game()
+    new_game()
     |> init_state()
     |> Player.play()
   end
@@ -12,5 +13,16 @@ defmodule TextClient.Interact do
       game_service: game,
       tally: Hangman.tally(game)
     }
+  end
+
+  defp new_game() do
+    Node.connect(@hangman_server)
+
+    :rpc.call(
+      @hangman_server,
+      Hangman,
+      :new_game,
+      []
+    )
   end
 end
